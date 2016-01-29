9b9ab5ee4e84e0e0348c252533f3894825bfa13d
SamEureka <sameureka@me.com>
2016-01-29 11:59:53 -0500

    draft styling and layout complete

--- app/App.js
+++ app/App.js
@@ -22,7 +22,7 @@ var styles = {
     color: '#222',
     resize: 'none',
     overflow: 'auto',
-    border: 0,
+    border: 0
   },
 pre: {
     position: 'fixed',
@@ -46,14 +46,14 @@ pre: {
     position: 'fixed',
     top: 0,
     color: '#666',
-    height: '3.3rem',
+    height: '3rem',
     width: '100vw',
-    fontFamily: 'Brandon-Grotesque',
-    fontSize: '2.1rem',
+    fontFamily: 'Varela',
+    fontSize: '2rem',
     paddingLeft: '0.5rem',
-    paddingTop: '0.2rem',
+    paddingTop: '0.5rem'
   },
-  footer: {
+  error: {
     position: 'fixed',
     left: 0,
     bottom: 2,
@@ -66,11 +66,11 @@ pre: {
     borderTop: '1px dotted red',
     borderRight: '1px dotted red',
     borderBottom: '1px dotted red',
-    zIndex: 1,
+    zIndex: 1
     },
   cont: {
-    height: '100vh',
-    display: 'flex',
+    height: '100vh', 
+    display: 'flex'
   },
   blue: {
     color: 'steelblue'
@@ -79,12 +79,25 @@ pre: {
     color: 'rgba(225, 55, 55,0.5)'
   },
   green: {
-    color: 'rgba(70,230,180,0.2)'
+    color: 'rgba(50,250,55,0.2)'
   },
   pick: {
     position: 'fixed',
     right: '1rem',
-    top: '1rem',
+    top: '0.7rem',
+    fontFamily: 'Lato',
+    fontSize: '1.2rem',
+    padding: '0.2rem',
+    backgroundColor: 'rgba(240,240,240,0.8)',
+  },
+  footer: {
+    position: 'fixed',
+    bottom: '0.8rem',
+    right: '0.8rem',
+    textDecoration: 'none',
+    color: 'rgba(55,55,55,0.6)',
+    fontFamily: 'Lato',
+    fontSize: '0.8rem',
   }
 };
 
@@ -118,20 +131,20 @@ class App extends React.Component {
       <header style={styles.header}>
         <i style={styles.red} className="fa fa-code"></i>
         <i style={styles.blue} className="fa fa-code"></i>
-        <span> transpilator</span>
-
-      <select style={styles.pick} name="select">
-        <option value="value1">Jade</option>
-        <option value="value2">CoffeeScript</option>
-        <option value="value3">ES2015 (Babel)</option>
-        <option value="value4">React/JSX (Babel)</option>
-
-</select></header>
+        <span>{' '}transpilator</span>
+        <div style={styles.pick}> Select <i className='fa fa-caret-down'></i></div>
+      </header>
       <div className="container" style={styles.cont}>
-          <textarea id="boom" style={styles.text} placeholder={this.state.in}
+        <textarea id="boom" style={styles.text} placeholder={this.state.in}
           onChange={this.update}></textarea>
-          <pre style={styles.pre}>{this.state.out}</pre>
-            <footer style={styles.footer}>{this.state.err}</footer>
+        <pre style={styles.pre}>{this.state.out}</pre>
+          <div style={styles.error}>{this.state.err}</div>
+        <div style={styles.footer}>
+            Made with <a href="http://en.wikipedia.org/wiki/Love" className='fa fa-heart-o fa-lg'></a> by {' '}
+            <a href="http://sam.dennon.me">Sam Dennon</a> & <a href="http://mattborn.com/">Matt Born</a> - {' '}
+            <a href="http://creativecommons.org/licenses/by/4.0/" className='fa fa-creative-commons fa-lg'></a> - Hosted by {'  '}
+            <a className="fa fa-github-alt fa-lg" href="http://www.github.com"> {'  '}Github</a> 
+        </div>
       </div>
     </div>
     )
